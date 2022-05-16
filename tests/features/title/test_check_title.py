82cfc56a1dcaabce30883ce6d2e90486a6d308c1
renardelyon <renard.elyon.r@gmail.com>
2022-05-16 22:18:01 +0700

    Write unit test for title

---
+++ tests/features/title/test_check_title.py
@@ -0,0 +1,21 @@
+import pytest
+from grplot.features.title.check_title import check_title
+import matplotlib as mpl
+
+@pytest.mark.parametrize('input,expected', [
+    ((None, None, None), (None)),
+    (('halo', None, None), ('halo')),
+    ((1, None, None), (None)),
+])
+def test_check_title_is_None(_ax: mpl.axes._axes.Axes, input: tuple[any, any, any], expected: any):
+    try:
+        axes = check_title(_ax, *input)
+    except:
+        with pytest.raises(Exception):
+            check_title(_ax, *input)
+    else:
+        if input[1] is None :
+            assert axes == _ax
+        else:
+            _ax.get_title == expected
+        
