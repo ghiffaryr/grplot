41cc04abc1cfaec989870de946d8a5f7c8685af5
ghiffaryr <grifqialdi@gmail.com>
2022-05-22 10:46:49 +0700

    tests: downgrade code to python 3.6 support

--- tests/utils/test_strtoarray.py
+++ tests/utils/test_strtoarray.py
@@ -1,14 +1,13 @@
 from grplot.utils.strtoarray import strtoarray
 import numpy as np
 import pytest
-from typing import Union
 
 @pytest.mark.parametrize('input,expected', [
     (('abe', 'jeanne'), (np.array(['abe' for i in range(6)]), 'jeanne')),
     (([1, 2, 3], 'jeanne'), ([1, 2, 3], np.array(['jeanne' for i in range(3)]))),
     (([1, 2, 3], [1, 2, 3]), ([1, 2, 3], [1, 2, 3])) 
 ])
-def test_strtoarray(input: tuple[Union[list, str], Union[list, str]], expected: tuple[any, any]): 
+def test_strtoarray(input: tuple, expected: tuple): 
     new_array = strtoarray(*input)
     for result, expectedOutput in zip(new_array, expected):
         if type(expectedOutput) == np.ndarray:
