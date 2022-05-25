0d05ff6a2eeeb7fe92967bfdf03ce8202d3b760c
renardelyon <renard.elyon.r@gmail.com>
2022-05-25 07:42:34 +0700

    create unit test for label_add

---
+++ tests/features/add/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+import matplotlib.pyplot as plt
+import numpy as np
+
+@pytest.fixture
+def _ax(request):
+    x = np.arange(0, 10, 0.1)
+    y = np.sin(x)
+
+    ax = plt.subplot(221)
+    ax.plot(x,y)
+
+    xlabel, ylabel = request.param
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    return ax
