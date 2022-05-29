b42d34c4e476d4ed8de3209ad1c0e7f6d8659d82
renardelyon <renard.elyon.r@gmail.com>
2022-05-29 19:51:37 +0700

    Create unit test for log

---
+++ tests/features/log/conftest.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pytest
+
+@pytest.fixture
+def _ax():
+    x = np.arange(0, 10, 0.1)
+    y = np.sin(x)
+
+    ax = plt.subplot(221)
+    ax.plot(x,y)
+    yield ax
+    plt.close()
