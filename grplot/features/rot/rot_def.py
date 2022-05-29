d57a909fd6210916e2650f4fec616e0513ee4a43
ghiffaryr <grifqialdi@gmail.com>
2022-05-29 14:37:10 +0700

    grplot/features/rot/rot_def: only support float

--- grplot/features/rot/rot_def.py
+++ grplot/features/rot/rot_def.py
@@ -1,12 +1,12 @@
 def rot_def(ax, axis, rot):
     if axis == 'x':
-        if (type(rot) == int) or (rot in ['horizontal', 'vertical']):
+        if type(rot) == float:
             for label in ax.get_xticklabels():
                 label.set_rotation(rot)
         else:
             raise Exception('Unknown rot argument!')
     elif axis == 'y':
-        if (type(rot) == int) or (rot in ['horizontal', 'vertical']):
+        if type(rot) == float:
             for label in ax.get_yticklabels():
                 label.set_rotation(rot)
         else:
