5c97083634be081b35a06cd12c778ec447354c3c
Renard Elyon Imawanto <79710053+renardelyon@users.noreply.github.com>
2022-06-04 21:10:12 +0700

    Update dt_def.py

--- grplot/features/dt/dt_def.py
+++ grplot/features/dt/dt_def.py
@@ -3,12 +3,12 @@ import matplotlib.dates as mdates
 
 def dt_def(ax, axis, dt):
     if axis == 'x':
-        if type(dt) != str:
+        if type(dt) == str:
             ax.xaxis.set_major_formatter(mdates.DateFormatter(dt))
         else:
             raise Exception('Unknown dt argument!')
     elif axis == 'y':
-        if type(dt) != str:
+        if type(dt) == str:
             ax.yaxis.set_major_formatter(mdates.DateFormatter(dt))
         else:
             raise Exception('Unknown dt argument!')
