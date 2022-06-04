cc08e64a11d96332ca221799eaf69240f47302ff
renardelyon <renard.elyon.r@gmail.com>
2022-06-04 20:16:25 +0700

    fix handle exception

--- grplot/features/dt/dt_def.py
+++ grplot/features/dt/dt_def.py
@@ -3,14 +3,14 @@ import matplotlib.dates as mdates
 
 def dt_def(ax, axis, dt):
     if axis == 'x':
-        try:
+        if type(dt) != str:
             ax.xaxis.set_major_formatter(mdates.DateFormatter(dt))
-        except:
+        else:
             raise Exception('Unknown dt argument!')
     elif axis == 'y':
-        try:
+        if type(dt) != str:
             ax.yaxis.set_major_formatter(mdates.DateFormatter(dt))
-        except:
+        else:
             raise Exception('Unknown dt argument!')
     else:
         raise Exception('Unsupported axis!')
