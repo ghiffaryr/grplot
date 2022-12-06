dfc7ef0b7e50a1791877c17db9f7570a5327000b
renardelyon <renard.elyon.r@gmail.com>
2022-12-06 20:13:26 +0700

    fix unit test cases

--- tests/features/rot/test_rot_type.py
+++ tests/features/rot/test_rot_type.py
@@ -3,11 +3,11 @@ from grplot.utils.arg_axis_ax_type import arg_axis_ax_type
 import pytest
 
 @pytest.mark.parametrize('input,expected', [
-    ([None, None, None, None], 0),
+    ([None, None, None, None], 90),
     (['x', 90, None, None], 90),
     (['x', 'hola', None, None], None)
 ])
-def test_rot_type(_ax: any, input: list[any], expected: any):
+def test_rot_type(_ax: any, input: list, expected: any):
     try:
         rotated_ax = rot_type(_ax, *input)
     except:
