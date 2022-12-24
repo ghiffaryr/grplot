b9c17de18bb53ddf39b52f6d7d08e3e091e11380
renardelyon <renard.elyon.r@gmail.com>
2022-12-24 10:16:39 +0700

    Update statdesc unit test

--- tests/features/statdesc/test_statdesc_multi_def.py
+++ tests/features/statdesc/test_statdesc_multi_def.py
@@ -1,6 +1,4 @@
 from grplot.features.statdesc.statdesc_multi_def import statdesc_multi_def
-import numpy as np
-import pandas as pd
 import pytest
 
 statdesc_inputs = [
@@ -30,29 +28,31 @@ input_for_exception_test = [
         {
             'axislabel': 'x',
             'statdesc': statdesc_input,
-            'sep':[],
+            'sep': [],
             'axis':'x',
             'axes': None,
             'add': []
-        },
-        None
+        }
     )
     for statdesc_input in statdesc_inputs
 ]
 
-@pytest.mark.parametrize('_ax, dataframe, input, expected', [
-    (
-        (0, 0),
-        ['other'],
-        {
-            'axislabel': 'x',
-            'statdesc':'general',
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        [
+
+class TestStatdescMultiDef:
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 0), ['other'])], indirect=['_ax', 'dataframe'])
+    def test_statdesc_general(self, _ax, dataframe):
+        ax = statdesc_multi_def(
+            ax=_ax,
+            df=dataframe,
+            axislabel='x',
+            statdesc='general',
+            sep='.c',
+            axis='x',
+            axes=None,
+            add=None
+        )
+
+        expected = [
             ('count', '6'),
             ('non zero count', '5'),
             ('unique count', '6'),
@@ -65,18 +65,26 @@ input_for_exception_test = [
             ('q3', '3,75'),
             ('max', '5,00'),
         ]
-    ), (
-        (1, 0),
-        ['numpy'],
-        {
-            'axislabel': 'x',
-            'statdesc':'boxplot',
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        [
+
+        for line, expected_val in zip(ax.get_lines(), expected):
+            label = line.get_label().split(':')
+            assert label[0] == expected_val[0]
+            assert label[1].strip() == expected_val[1]
+
+    @pytest.mark.parametrize('_ax, dataframe', [((1, 0), ['numpy'])], indirect=['_ax', 'dataframe'])
+    def test_statdesc_boxplot(self, _ax, dataframe):
+        ax = statdesc_multi_def(
+            ax=_ax,
+            df=dataframe,
+            axislabel='x',
+            statdesc='boxplot',
+            sep='.c',
+            axis='x',
+            axes=None,
+            add=None
+        )
+
+        expected = [
             ('min', '0'),
             ('lower whisker', '0'),
             ('q1', '1,25'),
@@ -88,73 +96,98 @@ input_for_exception_test = [
             ('upper whisker', '5,00'),
             ('max', '5,00'),
         ]
-    ), (
-        (0, 1),
-        ['other'],
-        {
-            'axislabel': 'x',
-            'statdesc':'pct1+pct5+pct95+pct99+yolo',
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        [
+
+        for line, expected_val in zip(ax.get_lines(), expected):
+            label = line.get_label().split(':')
+            assert label[0] == expected_val[0]
+            assert label[1].strip() == expected_val[1]
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other'])], indirect=['_ax', 'dataframe'])
+    def test_statdesc_else(self, _ax, dataframe):
+        ax = statdesc_multi_def(
+            ax=_ax,
+            df=dataframe,
+            axislabel='x',
+            statdesc='pct1+pct5+pct95+pct99+yolo',
+            sep='.c',
+            axis='x',
+            axes=None,
+            add=None
+        )
+
+        expected = [
             ('1st pct', '0,05'),
             ('5th pct', '0,25'),
             ('95th pct', '4,75'),
             ('99th pct', '4,95'),
         ]
-    ), (
-        (0, 1),
-        ['other', 'hola'],
-        {
-            'axislabel': 'x',
-            'statdesc':'',
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        None
-    ), (
-        (0, 1),
-        ['other', [1, None, 2]],
-        {
-            'axislabel': 'x',
-            'statdesc':'count',
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        [
-            ('count', '3')
-        ]
-    ), (
-        (0, 1),
-        ['other'],
-        {
-            'axislabel': 'x',
-            'statdesc': [],
-            'sep':'.c',
-            'axis':'x',
-            'axes': None,
-            'add': None
-        },
-        None
-    ),
-    *input_for_exception_test
-], indirect=['_ax', 'dataframe'])
-def test_statdesc_multi_def(_ax, dataframe, input, expected):
-    print(dataframe)
-    try:
-        ax = statdesc_multi_def(ax=_ax, df=dataframe, **input)
-    except:
-        with pytest.raises(Exception):
-            statdesc_multi_def(ax=_ax, df=dataframe, **input)
-    else:
+
         for line, expected_val in zip(ax.get_lines(), expected):
             label = line.get_label().split(':')
             assert label[0] == expected_val[0]
             assert label[1].strip() == expected_val[1]
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other', 'hola'])], indirect=['_ax', 'dataframe'])
+    def test_df_value_unsupported(self, _ax, dataframe):
+        with pytest.raises(Exception) as exc_info:
+            statdesc_multi_def(
+                ax=_ax,
+                df=dataframe,
+                axislabel='x',
+                statdesc='',
+                sep='.c',
+                axis='x',
+                axes=None,
+                add=None
+            )
+
+        assert 'Unsupported dtype' in str(exc_info.value)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other', [1, None, 2]])], indirect=['_ax', 'dataframe'])
+    def test_df_value_contains_none(self, _ax, dataframe):
+        ax = statdesc_multi_def(
+            ax=_ax,
+            df=dataframe,
+            axislabel='x',
+            statdesc='count',
+            sep='.c',
+            axis='x',
+            axes=None,
+            add=None
+        )
+
+        expected = [
+             ('count', '3')
+        ]
+
+        for line, expected_val in zip(ax.get_lines(), expected):
+            label = line.get_label().split(':')
+            assert label[0] == expected_val[0]
+            assert label[1].strip() == expected_val[1]
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other'])], indirect=['_ax', 'dataframe'])
+    def test_statdesc_argument_unknown(self, _ax, dataframe):
+        with pytest.raises(Exception) as exc_info:
+            statdesc_multi_def(
+                ax=_ax,
+                df=dataframe,
+                axislabel='x',
+                statdesc=[],
+                sep='.c',
+                axis='x',
+                axes=None,
+                add=None
+            )
+
+        assert 'Unknown statdesc argument' in str(exc_info.value)
+
+    @pytest.mark.parametrize('_ax, dataframe, input', [*input_for_exception_test], indirect=['_ax', 'dataframe'])
+    def test_label_not_in_dataframe(self, _ax, dataframe, input):
+        with pytest.raises(Exception) as exc_info:
+            statdesc_multi_def(
+                ax=_ax,
+                df=dataframe,
+                **input
+            )
+
+        assert 'Label not in the dataframe' in str(exc_info.value)
