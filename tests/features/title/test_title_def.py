1ce33edd507c5702314c552197d21dc1bc3bfe84
renardelyon <renard.elyon.r@gmail.com>
2022-12-24 11:25:56 +0700

    Update unit test title

--- tests/features/title/test_title_def.py
+++ tests/features/title/test_title_def.py
@@ -1,17 +1,18 @@
 import pytest
 from grplot.features.title.title_def import title_def
-import matplotlib as mpl
 
-@pytest.mark.parametrize('input,expected', [
-    (('halo', None), ('halo')),
-    (('halo', 15), ('halo')),
-    (('halo', 'amigos'), (None))
-])
-def test_title_def(_ax: mpl.axes._axes.Axes, input: tuple, expected: any):
-    try:
+
+class TestTitleDef:
+    @pytest.mark.parametrize('input', [
+        (('halo', None)),
+        (('halo', 15)),
+    ])
+    def test_set_title(self, _ax, input):
         title_def(_ax, *input)
-    except:
-        with pytest.raises(Exception):
-            title_def(_ax, *input)
-    else:
-        assert _ax.get_title() == expected
+        assert _ax.get_title() == 'halo'
+
+    def test_unknown_fontsize_argument(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            title_def(_ax, title='hola', title_fontsize='y')
+
+        assert str(exc_info.value) == 'Unknown title fontsize argument!'
