e3dd40293ea62b77096269599ca2ecdcf9fc3580
renardelyon <renard.elyon.r@gmail.com>
2022-12-24 10:16:39 +0700

    Update statdesc unit test

--- tests/features/statdesc/test_check_statdesc.py
+++ tests/features/statdesc/test_check_statdesc.py
@@ -4,205 +4,249 @@ import pytest
 
 data = [0, 100000, 200000, 300000, 400000, 500000]
 
-@pytest.mark.parametrize('_ax, dataframe, input, expected', [
-    (
-        (0, 0),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': 'mean',
-            'ystatdesc': 'mean',
-            'sep': '.c',
-            'xsep': '.c',
-            'ysep': '.c',
-            'add': 'Rp(_)',
-            'xadd': 'Rp(_)',
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': 'Rp(_)',
-            'axes': None
-        },
-        (
+
+class TestCheckStatdesc:
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 0), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_all_params_not_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc='mean',
+            ystatdesc='mean',
+            sep='.c',
+            add='$(_)',
+            xsep='.c',
+            ysep='.c',
+            axes='x',
+            xadd='Rp(_)',
+            xaxislabel='x',
+            yadd='Rp(_)',
+            yaxislabel='y',
+        )
+
+        expected = [
             ('mean: Rp250.000,00', ([250000.0, 250000.0], [0, 1])),
             ('mean: Rp250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (0, 0),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': 'mean',
-            'ystatdesc': 'mean',
-            'sep': '.c',
-            'xsep': '.c',
-            'ysep': '.c',
-            'add': '$(_)',
-            'xadd': None,
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': None,
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (1, 0),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': 'mean',
-            'ystatdesc': 'mean',
-            'sep': '.c',
-            'xsep': None,
-            'ysep': None,
-            'add': '$(_)',
-            'xadd': '$(_)',
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': 'Rp.(_)',
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: Rp.250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (1, 0),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': 'mean',
-            'ystatdesc': 'mean',
-            'sep': '.c',
-            'xsep': None,
-            'ysep': None,
-            'add': '$(_)',
-            'xadd': None,
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': None,
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (0, 1),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': None,
-            'ystatdesc': None,
-            'sep': '.c',
-            'xsep': '.c',
-            'ysep': '.c',
-            'add': '$(_)',
-            'xadd': 'ok(_)',
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': '$(_)',
-            'axes': None
-        },
-        (
-            ('mean: ok250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (0, 1),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': None,
-            'ystatdesc': None,
-            'sep': '.c',
-            'xsep': '.c',
-            'ysep': '.c',
-            'add': '$(_)',
-            'xadd': None,
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': None,
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (0, 1),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': None,
-            'ystatdesc': None,
-            'sep': '.c',
-            'xsep': None,
-            'ysep': None,
-            'add': '$(_)',
-            'xadd': '$(_)',
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': 'ok(_)',
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: ok250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (0, 1),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': None,
-            'ystatdesc': None,
-            'sep': '.c',
-            'xsep': None,
-            'ysep': None,
-            'add': '$(_)',
-            'xadd': None,
-            'xaxislabel': 'x',
-            'yaxislabel': 'y',
-            'yadd': None,
-            'axes': None
-        },
-        (
-            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
-            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
-        )
-    ), (
-        (1, 1),
-        ['other', data],
-        {
-            'statdesc': 'mean',
-            'xstatdesc': None,
-            'ystatdesc': None,
-            'sep': '.c',
-            'xsep': None,
-            'ysep': None,
-            'add': '$(_)',
-            'xadd': None,
-            'xaxislabel': None,
-            'yaxislabel': None,
-            'yadd': None,
-            'axes': None
-        },
-        None
-    )
-], indirect=['_ax', 'dataframe'])
-def test_check_statdesc(_ax, dataframe, input, expected):
-    ax = check_statdesc(
-        df=dataframe,
-        ax=_ax,
-        **input
-    )
+        ]
 
-    if len(ax.get_lines()) > 0:
         for line, (label, data) in zip(ax.get_lines(), expected):
             assert line.get_label() == label
             np.testing.assert_array_equal(line.get_data(), data)
-    else:
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 0), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_and_y_add_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc='mean',
+            ystatdesc='mean',
+            sep='.c',
+            add='$(_)',
+            xsep='.c',
+            ysep='.c',
+            axes='x',
+            xadd=None,
+            xaxislabel='x',
+            yadd=None,
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((1, 0), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_and_y_sep_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc='mean',
+            ystatdesc='mean',
+            sep='.c',
+            add='$(_)',
+            xsep=None,
+            ysep=None,
+            axes='x',
+            xadd='$(_)',
+            xaxislabel='x',
+            yadd='Rp.(_)',
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: Rp.250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((1, 0), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_y_sep_add_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc='mean',
+            ystatdesc='mean',
+            sep='.c',
+            add='$(_)',
+            xsep=None,
+            ysep=None,
+            axes='x',
+            xadd=None,
+            xaxislabel='x',
+            yadd=None,
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((1, 0), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_y_statdesc_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc=None,
+            ystatdesc=None,
+            sep='.c',
+            add='$(_)',
+            xsep='.c',
+            ysep='.c',
+            axes='x',
+            xadd='ok(_)',
+            xaxislabel='x',
+            yadd='$(_)',
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: ok250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_y_statdesc_add_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc=None,
+            ystatdesc=None,
+            sep='.c',
+            add='$(_)',
+            xsep='.c',
+            ysep='.c',
+            axes='x',
+            xadd=None,
+            xaxislabel='x',
+            yadd=None,
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_y_statdesc_sep_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc=None,
+            ystatdesc=None,
+            sep='.c',
+            add='$(_)',
+            xsep=None,
+            ysep=None,
+            axes='x',
+            xadd='$(_)',
+            xaxislabel='x',
+            yadd='ok(_)',
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: ok250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((0, 1), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_x_y_statdesc_sep_add_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc=None,
+            ystatdesc=None,
+            sep='.c',
+            add='$(_)',
+            xsep=None,
+            ysep=None,
+            axes='x',
+            xadd=None,
+            xaxislabel='x',
+            yadd=None,
+            yaxislabel='y',
+        )
+
+        expected = [
+            ('mean: $250.000,00', ([250000.0, 250000.0], [0, 1])),
+            ('mean: $250.000,00', ([0, 1], [250000.0, 250000.0])),
+        ]
+
+        for line, (label, data) in zip(ax.get_lines(), expected):
+            assert line.get_label() == label
+            np.testing.assert_array_equal(line.get_data(), data)
+
+    @pytest.mark.parametrize('_ax, dataframe', [((1, 1), ['other', data])], indirect=['_ax', 'dataframe'])
+    def test_axis_label_is_none(self, _ax, dataframe):
+        ax = check_statdesc(
+            df=dataframe,
+            ax=_ax,
+            statdesc='mean',
+            xstatdesc=None,
+            ystatdesc=None,
+            sep='.c',
+            add='$(_)',
+            xsep=None,
+            ysep=None,
+            axes=None,
+            xadd=None,
+            xaxislabel=None,
+            yadd=None,
+            yaxislabel=None,
+        )
+
         assert len(ax.get_lines()) == 0
