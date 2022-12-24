c0f616566ea32b8aa6eb589f3650829dbc602f63
renardelyon <renard.elyon.r@gmail.com>
2022-12-25 06:36:25 +0700

    Update unit test log

--- tests/features/log/test_log_def.py
+++ tests/features/log/test_log_def.py
@@ -2,48 +2,41 @@ from grplot.features.log.log_def import log_def
 import numpy
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'axis': 'x',
-            'log': 'logit'
-        },
-        [0.01, 0.1,  0.5,  0.9,  0.99]
-    ), (
-        {
-            'axis': 'y',
-            'log': 'logit'
-        },
-        [0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999, 0.9999]
-    ), (
-        {
-            'axis': 'x',
-            'log': 'alakazam'
-        },
-        None
-    ), (
-        {
-            'axis': 'y',
-            'log': 'alakazam'
-        },
-        None
-    ), (
-        {
-            'axis': 'sas',
-            'log': 'alakazam'
-        },
-        None
-    )
-])
-def test_log_def(_ax, input, expected):
-    try:
-        ax = log_def(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            log_def(_ax, **input)
-    else:
-        if input['axis'] == 'x':
-            numpy.testing.assert_array_equal(ax.get_xticks(), expected)
-        
-        if input['axis'] == 'y':
-            numpy.testing.assert_array_equal(ax.get_yticks(), expected)
+
+class TestLogDef:
+    def test_scale_x(self, _ax):
+        ax = log_def(
+            ax=_ax,
+            axis='x',
+            log='logit'
+        )
+        numpy.testing.assert_array_equal(ax.get_xticks(), [0.01, 0.1,  0.5,  0.9,  0.99])
+
+    def test_scale_y(self, _ax):
+        ax = log_def(
+            ax=_ax,
+            axis='y',
+            log='logit'
+        )
+        numpy.testing.assert_array_equal(ax.get_yticks(), [0.001, 0.01, 0.1, 0.5, 0.9, 0.99, 0.999, 0.9999])
+
+    @pytest.mark.parametrize('axis', [('x'), ('y')])
+    def test_unknown_log_argument(self, _ax, axis):
+        with pytest.raises(Exception) as exc_info:
+            log_def(
+                ax=_ax,
+                axis=axis,
+                log='boom'
+            )
+
+        assert str(exc_info.value) == 'Unknown log argument!'
+
+    def test_unsupported_axis(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            log_def(
+                ax=_ax,
+                axis='y2k',
+                log='logit'
+            )
+
+        assert str(exc_info.value) == 'Unsupported axis!'
--- tests/features/log/test_log_type.py
+++ tests/features/log/test_log_type.py
@@ -2,38 +2,38 @@ from grplot.features.log.log_type import log_type
 import numpy
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'axis': 'x',
-            'log': None,
-            'axislabel': 'time',
-            'axes': None
-        },
-        [-2.5,  0. ,  2.5,  5. ,  7.5, 10. , 12.5]
-    ), (
-        {
-            'axis': 'x',
-            'log': 'logit',
-            'axislabel': 'time',
-            'axes': None
-        },
-        [0.01, 0.1,  0.5,  0.9,  0.99]
-    ), (
-        {
-            'axis': 'x',
-            'log': [],
-            'axislabel': 'time',
-            'axes': None
-        },
-        None
-    )
-])
-def test_log_type(_ax, input, expected):
-    try:
-        ax = log_type(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            log_type(_ax, **input)
-    else:
-        numpy.testing.assert_array_equal(ax.get_xticks(), expected)
+
+class TestLogType:
+    def test_log_value_is_none(self, _ax):
+        ax = log_type(
+            ax=_ax,
+            axes=None,
+            axis='x',
+            axislabel='time',
+            log=None
+        )
+
+        numpy.testing.assert_array_equal(ax.get_xticks(), [-2.5,  0.,  2.5,  5., 7.5, 10., 12.5])
+
+    def test_log_has_str_value(self, _ax):
+        ax = log_type(
+            ax=_ax,
+            axes=None,
+            axis='x',
+            axislabel='time',
+            log='logit'
+        )
+
+        numpy.testing.assert_array_equal(ax.get_xticks(), [0.01, 0.1,  0.5,  0.9,  0.99])
+
+    def test_unknown_log_argument(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            log_type(
+                ax=_ax,
+                axes=None,
+                axis='x',
+                axislabel='time',
+                log=[]
+            )
+
+        assert str(exc_info.value) == 'Unknown log argument!'
