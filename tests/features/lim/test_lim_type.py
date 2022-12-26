813b39180ac9fc1a47989683398d7101302f475c
renardelyon <renard.elyon.r@gmail.com>
2022-12-26 17:23:55 +0700

    Update unit test lim

--- tests/features/lim/test_lim_type.py
+++ tests/features/lim/test_lim_type.py
@@ -1,39 +1,39 @@
 from grplot.features.lim.lim_type import lim_type
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'axis': 'x',
-            'lim': None,
-            'axislabel': 'time',
-            'axes': None
-        },
-        6
-    ), (
-        {
-            'axis': 'x',
-            'lim': [0, 3],
-            'axislabel': 'time',
-            'axes': None
-        },
-        3
-    ), (
-        {
-            'axis': 'x',
-            'lim': 'sadds',
-            'axislabel': 'time',
-            'axes': None
-        },
-        3
-    )
-])
-def test_lim_type(_ax, input, expected):
-    try:
-        ax = lim_type(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            lim_type(_ax, *input)
-    else:
-        assert ax.get_xticks()[-1] == expected
 
+class TestLimType:
+    def test_lim_is_none(self, _ax):
+        ax = lim_type(
+            ax=_ax,
+            axis='x',
+            axes=None,
+            axislabel='time',
+            lim=None
+        )
+
+        assert ax.get_xticks()[-1] == 6
+
+    def test_lim_has_array_value(self, _ax):
+        lim = [0, 3]
+        ax = lim_type(
+            ax=_ax,
+            axis='x',
+            axes=None,
+            axislabel='time',
+            lim=lim
+        )
+
+        assert ax.get_xticks()[-1] == lim[-1]
+
+    def test_unknown_lim_argument(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            lim_type(
+                ax=_ax,
+                axis='x',
+                axes=None,
+                axislabel='time',
+                lim='yo'
+            )
+
+        assert str(exc_info.value) == 'Unknown lim argument!'
