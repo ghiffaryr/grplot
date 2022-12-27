ce010e34edf8c8bde57c1b44c52029fc243619d8
renardelyon <renard.elyon.r@gmail.com>
2022-12-27 20:33:17 +0700

    Update unit test dt

--- tests/features/dt/test_dt_def.py
+++ tests/features/dt/test_dt_def.py
@@ -1,37 +1,37 @@
 from grplot.features.dt.dt_def import dt_def
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'axis': 'x',
-            'dt': '%Y/%m/%d'
-        },
-        '2022/01/01'
-    ), (
-        {
-            'axis': 'y',
-            'dt': '%Y/%m/%d'
-        },
-        '1970/01/01'
-    ), (
-        {
-            'axis': 'hola',
-            'dt': '%Y/%m/%d'
-        },
-        None
-    ), 
-])
-def test_dt_def(_ax, input, expected):
-    try:
+
+class TestDtDef:
+    def test_set_xaxis_formatter(self, _ax):
         ax, fig = _ax
-        ax = dt_def(ax, **input)
+        ax = dt_def(
+            ax=ax,
+            axis='x',
+            dt='%Y/%m/%d'
+        )
         fig.canvas.draw()
-    except:
-        with pytest.raises(Exception):
-            dt_def(_ax, **input)
-    else:
-        if input['axis'] == 'x':
-            assert ax.get_xticklabels()[0].get_text() == expected
-        if input['axis'] == 'y':
-            assert ax.get_yticklabels()[0].get_text() == expected
+
+        assert ax.get_xticklabels()[0].get_text() == '2022/01/01'
+
+    def test_set_yaxis_formatter(self, _ax):
+        ax, fig = _ax
+        ax = dt_def(
+            ax=ax,
+            axis='y',
+            dt='%Y/%m/%d'
+        )
+        fig.canvas.draw()
+
+        assert ax.get_yticklabels()[0].get_text() == '1970/01/01'
+
+    def test_unsupported_axis(self, _ax):
+        ax, _ = _ax
+        with pytest.raises(Exception) as exc_info:
+            dt_def(
+                ax=ax,
+                axis='kk',
+                dt='%Y/%m/%d'
+            )
+
+        assert str(exc_info.value) == 'Unsupported axis!'
