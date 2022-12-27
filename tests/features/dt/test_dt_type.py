441bb03d7ffe0f6e24f24b653eb3a9abbad9d3f5
renardelyon <renard.elyon.r@gmail.com>
2022-12-27 20:33:17 +0700

    Update unit test dt

--- tests/features/dt/test_dt_type.py
+++ tests/features/dt/test_dt_type.py
@@ -1,40 +1,43 @@
 from grplot.features.dt.dt_type import dt_type
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'axis': 'x',
-            'dt': None,
-            'axislabel': 'x',
-            'axes': None
-        },
-        '2022-01-01'
-    ), (
-        {
-            'axis': 'x',
-            'dt': '%Y/%m/%d',
-            'axislabel': 'x',
-            'axes': None
-        },
-        '2022/01/01'
-    ), (
-        {
-            'axis': 'x',
-            'dt': [],
-            'axislabel': 'x',
-            'axes': None
-        },
-        None
-    )
-])
-def test_dt_type(_ax, input, expected):
-    try:
+
+class TestDtType:
+    def test_dt_is_none(self, _ax):
         ax, fig = _ax
-        ax = dt_type(ax, **input)
+        ax = dt_type(
+            ax=ax,
+            axes=None,
+            dt=None,
+            axis='x',
+            axislabel='x'
+        )
         fig.canvas.draw()
-    except:
-        with pytest.raises(Exception):
-            dt_type(_ax, **input)
-    else:
-        assert ax.get_xticklabels()[0].get_text() == expected
+
+        assert ax.get_xticklabels()[0].get_text() == '2022-01-01'
+
+    def test_dt_has_value(self, _ax):
+        ax, fig = _ax
+        ax = dt_type(
+            ax=ax,
+            axes=None,
+            dt='%Y/%m/%d',
+            axis='x',
+            axislabel='x'
+        )
+        fig.canvas.draw()
+
+        assert ax.get_xticklabels()[0].get_text() == '2022/01/01'
+
+    def test_unknown_dt_argument(self, _ax):
+        ax, _ = _ax
+        with pytest.raises(Exception) as exc_info:
+            dt_type(
+                ax=ax,
+                axes=None,
+                dt=[],
+                axis='x',
+                axislabel='x'
+            )
+
+        assert str(exc_info.value) == "Unknown dt argument!"
