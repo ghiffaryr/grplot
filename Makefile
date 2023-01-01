5d9a5ad3a34c6d60824e3ca7576862e1f0ef7813
renardelyon <renard.elyon.r@gmail.com>
2023-01-01 21:17:29 +0700

    Update unit test label add

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 export SHELL := /bin/bash
 
+test-cov:
+	pytest --cov-config=.coveragerc --cov=grplot --cov-report term-missing --cov-report html
+
 test:
-	pytest
+	pytest --cov-config=.coveragerc --cov=grplot 
 
 lint:
 	flake8 --extend-ignore E501,W291 ./grplot ./tests
--- tests/features/add/label_add/test_label_add_def.py
+++ tests/features/add/label_add/test_label_add_def.py
@@ -2,79 +2,47 @@ from grplot.features.add.label_add.label_add_def import label_add_def
 import pytest
 
 
-@pytest.mark.parametrize('_ax, input, expected', [
-    (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': 'mantap(_)'
-        },
-        ['mantap(time)', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': 'huge _'
-        },
-        ['huge time', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': '_(ms)'
-        },
-        ['time(ms)', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': 'mantap(_)'
-        },
-        ['time', 'mantap(freq)']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': 'huge _'
-        },
-        ['time', 'huge freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': '_(Hz)'
-        },
-        ['time', 'freq(Hz)']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': 'mantap'
-        },
-            None
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': 'mantap'
-        },
-        None
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'halo',
-            'add': 'mantap'
-        },
-        None
+class TestLabelAddDef:
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_unsupported_axis(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            label_add_def(add="mantap", ax=_ax, axis="halo")
+
+        assert str(exc_info.value) == "Unsupported axis!"
+
+    @pytest.mark.parametrize(
+        "_ax, axis",
+        [(["time", "freq"], "x"), (["time", "freq"], "y")],
+        indirect=["_ax"],
     )
-], indirect=['_ax'])
-def test_label_add_def(_ax, input, expected):
-    try:
+    def test_unknown_label_argument(self, _ax, axis):
+        with pytest.raises(Exception) as exc_info:
+            label_add_def(ax=_ax, add="mantap", axis=axis)
+
+        assert str(exc_info.value) == "Unknown label add argument!"
+
+    @pytest.mark.parametrize(
+        "_ax, input, expected",
+        [
+            (["time", "freq"], {"axis": "x", "add": "mantap(_)"}, "mantap(time)"),
+            (["time", "freq"], {"axis": "x", "add": "huge _"}, "huge time"),
+            (["time", "freq"], {"axis": "x", "add": "_(ms)"}, "time(ms)"),
+        ],
+        indirect=["_ax"],
+    )
+    def test_add_prefix_suffix_in_x_label(self, _ax, input, expected):
         ax = label_add_def(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            label_add_def(_ax, **input)
-    else:
-        expected_xlabel, expected_ylabel = expected
-        assert ax.get_xlabel() == expected_xlabel
-        assert ax.get_ylabel() == expected_ylabel
+        assert ax.get_xlabel() == expected
+
+    @pytest.mark.parametrize(
+        "_ax, input, expected",
+        [
+            (["time", "freq"], {"axis": "y", "add": "mantap(_)"}, "mantap(freq)"),
+            (["time", "freq"], {"axis": "y", "add": "huge _"}, "huge freq"),
+            (["time", "freq"], {"axis": "y", "add": "_(Hz)"}, "freq(Hz)"),
+        ],
+        indirect=["_ax"],
+    )
+    def test_add_prefix_suffix_in_y_label(self, _ax, input, expected):
+        ax = label_add_def(_ax, **input)
+        assert ax.get_ylabel() == expected
