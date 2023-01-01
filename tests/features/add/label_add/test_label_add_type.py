480821da0f3e3599207aac2b6ac0244031ac3de7
renardelyon <renard.elyon.r@gmail.com>
2023-01-01 21:17:29 +0700

    Update unit test label add

--- tests/features/add/label_add/test_label_add_type.py
+++ tests/features/add/label_add/test_label_add_type.py
@@ -2,43 +2,24 @@ from grplot.features.add.label_add.label_add_type import label_add_type
 import pytest
 
 
-@pytest.mark.parametrize('_ax, input, expected', [
-    (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': None,
-            'axislabel': 'time',
-            'axes': None 
-        },
-        ['time', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': '_ (Hz)',
-            'axislabel': 'freq',
-            'axes': None 
-        },
-        ['time', 'freq (Hz)']
-    ), (
-         ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': [],
-            'axislabel': 'freq',
-            'axes': None 
-        },
-        None
-    )
-], indirect=['_ax'])
-def test_label_add_type(_ax, input, expected):
-    try:
-        ax = label_add_type(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            label_add_type(_ax, **input)
-    else:
-        expected_xlabel, expected_ylabel = expected
-        assert ax.get_xlabel() == expected_xlabel
-        assert ax.get_ylabel() == expected_ylabel
+class TestLabelAddType:
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_add_is_none(self, _ax):
+        ax = label_add_type(ax=_ax, add=None, axes=None, axis="x", axislabel="time")
+
+        assert ax.get_xlabel() == "time"
+        assert ax.get_ylabel() == "freq"
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_add_has_value(self, _ax):
+        ax = label_add_type(ax=_ax, add="_ (Hz)", axes=None, axis="y", axislabel="freq")
+
+        assert ax.get_xlabel() == "time"
+        assert ax.get_ylabel() == "freq (Hz)"
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_unknown_label_add_argument(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            label_add_type(ax=_ax, add=[], axes=None, axis="y", axislabel="freq")
+
+        assert str(exc_info.value) == "Unknown label add argument!"
