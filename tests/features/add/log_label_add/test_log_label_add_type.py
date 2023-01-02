6c1e037a35dedc39f1198d5f154674496f99ad79
renardelyon <renard.elyon.r@gmail.com>
2023-01-02 14:26:34 +0700

    Update unit test log label add

--- tests/features/add/log_label_add/test_log_label_add_type.py
+++ tests/features/add/log_label_add/test_log_label_add_type.py
@@ -1,43 +1,21 @@
 from grplot.features.add.log_label_add.log_label_add_type import log_label_add_type
 import pytest
 
-@pytest.mark.parametrize('_ax, input, expected', [
-    (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': None,
-            'axislabel': 'time',
-            'axes': None
-        },
-        ['time', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': 'log',
-            'axislabel': 'time',
-            'axes': None
-        },
-        ['log (time)', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': [],
-            'axislabel': 'time',
-            'axes': None
-        },
-        None
-    )
-], indirect=['_ax'])
-def test_log_label_add_type(_ax, input, expected):
-    try:
-        ax = log_label_add_type(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            log_label_add_type(_ax, **input)
-    else:
-        expected_xlabel, expected_ylabel = expected
-        assert ax.get_xlabel() == expected_xlabel
-        assert ax.get_ylabel() == expected_ylabel
+
+class TestLogLabelAddType:
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_add_is_none(self, _ax):
+        axislabel = "time"
+        ax = log_label_add_type(_ax, axis="x", add=None, axislabel=axislabel, axes=None)
+        assert ax.get_xlabel() == axislabel
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_add_has_value(self, _ax):
+        ax = log_label_add_type(_ax, axis="x", add="log", axislabel="time", axes=None)
+        assert ax.get_xlabel() == "log (time)"
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_unknown_log_label_argument(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            log_label_add_type(_ax, axis="x", add=[], axislabel="time", axes=None)
+        assert str(exc_info.value) == "Unknown log label add argument!"
