33dcbdeb0e48c96b549e0f603f9be9cb22c549ae
renardelyon <renard.elyon.r@gmail.com>
2023-01-02 14:26:34 +0700

    Update unit test log label add

--- tests/features/add/log_label_add/test_log_label_add_def.py
+++ tests/features/add/log_label_add/test_log_label_add_def.py
@@ -1,37 +1,21 @@
 from grplot.features.add.log_label_add.log_label_add_def import log_label_add_def
 import pytest
 
-@pytest.mark.parametrize('_ax, input, expected', [
-    (
-        ['time', 'freq'],
-        {
-            'axis': 'x',
-            'add': 'log'
-        },
-        ['log (time)', 'freq']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': 'y',
-            'add': 'log'
-        },
-        ['time', 'log (freq)']
-    ), (
-        ['time', 'freq'],
-        {
-            'axis': None,
-            'add': 'log'
-        },
-        None
-    )
-], indirect=['_ax'])
-def test_log_label_add_def(_ax, input, expected):
-    try:
-        ax = log_label_add_def(_ax, **input)
-    except:
-        with pytest.raises(Exception):
-            log_label_add_def(_ax, **input)
-    else:
-        expected_xlabel, expected_ylabel = expected
-        assert ax.get_xlabel() == expected_xlabel
-        assert ax.get_ylabel() == expected_ylabel
+
+class TestLogLabelAddDef:
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_set_x_label(self, _ax):
+        ax = log_label_add_def(_ax, axis="x", add="log")
+        assert ax.get_xlabel() == "log (time)"
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_set_y_label(self, _ax):
+        ax = log_label_add_def(_ax, axis="y", add="log")
+        assert ax.get_ylabel() == "log (freq)"
+
+    @pytest.mark.parametrize("_ax", [(["time", "freq"])], indirect=["_ax"])
+    def test_unsupported_axis(self, _ax):
+        with pytest.raises(Exception) as exc_info:
+            log_label_add_def(_ax, axis=None, add="log")
+
+        assert str(exc_info.value) == "Unsupported axis!"
