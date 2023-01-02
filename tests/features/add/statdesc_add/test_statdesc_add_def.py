62d1cae7b86a92e13e5bd43b29f7d132a52ee797
renardelyon <renard.elyon.r@gmail.com>
2023-01-02 17:22:07 +0700

    Update unit test statdesc add

--- tests/features/add/statdesc_add/test_statdesc_add_def.py
+++ tests/features/add/statdesc_add/test_statdesc_add_def.py
@@ -2,86 +2,38 @@ from ast import Num
 from grplot.features.add.statdesc_add.statdesc_add_def import statdesc_add_def
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'num': 10000,
-            'add': 'Rp (_)'
-        },
-        'Rp 10000'
-    ), (
-        {
-            'num': '-10000',
-            'add': 'Rp (_)'
-        },
-        '(Rp 10000)'
-    ), (
-        {
-            'num': 10000,
-            'add': '(_) km'
-        },
-        '10000 km'
-    ), (
-        {
-            'num': -10000,
-            'add': '(_) km'
-        },
-        '(10000 km)'
-    ), (
-        {
-            'num': 10000,
-            'add': 'huge (_) km'
-        },
-        'huge 10000 km'
-    ), (
-        {
-            'num': -10000,
-            'add': 'huge (_) km'
-        },
-        '(huge 10000 km)'
-    ), (
-        {
-            'num': 10000,
-            'add': 'Rp _'
-        },
-        'Rp 10000'
-    ), (
-        {
-            'num': -10000,
-            'add': 'Rp _'
-        },
-        '-Rp 10000'
-    ), (
-        {
-            'num': 10000,
-            'add': '_ km'
-        },
-        '10000 km'
-    ), (
-        {
-            'num': 10000,
-            'add': 'huge _ km'
-        },
-        'huge 10000 km'
-    ), (
-        {
-            'num': -10000,
-            'add': 'huge _ km'
-        },
-        '-huge 10000 km'
-    ), (
-        {
-            'num': 10000,
-            'add': 'skadnasjkdnaskdj'
-        },
-        None
+
+class TestStatdescAddDef:
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            ({"num": 10000, "add": "Rp (_)"}, "Rp 10000"),
+            ({"num": "-10000", "add": "Rp (_)"}, "(Rp 10000)"),
+            ({"num": 10000, "add": "(_) km"}, "10000 km"),
+            ({"num": -10000, "add": "(_) km"}, "(10000 km)"),
+            ({"num": 10000, "add": "huge (_) km"}, "huge 10000 km"),
+            ({"num": -10000, "add": "huge (_) km"}, "(huge 10000 km)"),
+        ],
     )
-])
-def test_statdesc_add_def(input, expected):
-    try:
+    def test_add_with_underscore_in_parenthesis(self, input, expected):
         num = statdesc_add_def(**input)
-    except:
-        with pytest.raises(Exception):
-            statdesc_add_def(**input)
-    else:
         assert num == expected
+
+    @pytest.mark.parametrize(
+        "input, expected",
+        [
+            ({"num": 10000, "add": "Rp _"}, "Rp 10000"),
+            ({"num": -10000, "add": "Rp _"}, "-Rp 10000"),
+            ({"num": 10000, "add": "_ km"}, "10000 km"),
+            ({"num": 10000, "add": "huge _ km"}, "huge 10000 km"),
+            ({"num": -10000, "add": "huge _ km"}, "-huge 10000 km"),
+        ],
+    )
+    def test_add_with_underscore(self, input, expected):
+        num = statdesc_add_def(**input)
+        assert num == expected
+
+    def test_unknown_statdesc_add_argument(self):
+        with pytest.raises(Exception) as exc_info:
+            statdesc_add_def(num=10000, add="skadnasjkdnaskdj")
+        assert str(exc_info.value) == "Unknown statdesc add argument!"
--- tests/features/add/statdesc_add/test_statdesc_add_type.py
+++ tests/features/add/statdesc_add/test_statdesc_add_type.py
@@ -1,38 +1,22 @@
 from grplot.features.add.statdesc_add.statdesc_add_type import statdesc_add_type
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'num': 10000,
-            'add': None,
-            'axislabel': 'time',
-            'axes': None
-        },
-        10000
-    ), (
-        {
-            'num': 10000,
-            'add': 'Rp (_)',
-            'axislabel': 'time',
-            'axes': None
-        },
-        'Rp 10000'
-    ), (
-        {
-            'num': 10000,
-            'add': [],
-            'axislabel': 'time',
-            'axes': None
-        },
-        None
-    )
-])
-def test_statdesc_add_type(input, expected):
-    try:
-        num = statdesc_add_type(**input)
-    except:
-        with pytest.raises(Exception):
-            statdesc_add_type(**input)
-    else:
-        assert num == expected
+
+class TestStatdescAddType:
+    def test_add_is_none(self):
+        num = statdesc_add_type(add=None, axislabel="time", num=10000, axes=None)
+        assert num == 10000
+
+    def test_add_has_value(self):
+        num = statdesc_add_type(add="Rp (_)", axislabel="time", num=10000, axes=None)
+        assert num == "Rp 10000"
+
+    def test_unknown_statdesc_add_argument(self):
+        with pytest.raises(Exception) as exc_info:
+            statdesc_add_type(
+                add=[],
+                axes=None,
+                axislabel="time",
+                num=10000,
+            )
+        assert str(exc_info.value) == "Unknown statdesc add argument!"
