dcaddb431c5317161cebe10d398fd958c5a0879e
renardelyon <renard.elyon.r@gmail.com>
2023-01-03 13:51:13 +0700

    Update unit test text add

--- tests/features/add/text_add/test_text_add_type.py
+++ tests/features/add/text_add/test_text_add_type.py
@@ -1,41 +1,44 @@
 from grplot.features.add.text_add.text_add_type import text_add_type
 import pytest
 
-@pytest.mark.parametrize('input, expected', [
-    (
-        {
-            'plot': 'pieplot',
-            'num': 10000,
-            'add': None,
-            'axislabel': 'time',
-            'axes': None
-        },
-        10000
-    ), (
-        {
-            'plot': 'line',
-            'num': 10000,
-            'add': 'Rp (_)',
-            'axislabel': 'time',
-            'axes': None
-        },
-        'Rp 10000'
-    ), (
-        {
-            'plot': 'pieplot',
-            'num': 10000,
-            'add': [],
-            'axislabel': 'time',
-            'axes': None
-        },
-        None
-    )
-])
-def test_text_add_type(input, expected):
-    try:
-        num = text_add_type(**input)
-    except:
-        with pytest.raises(Exception):
-            text_add_type(**input)
-    else:
-        assert num == expected
+
+class TestTextAddType:
+    def test_pieplot_add_is_none(self):
+        num = text_add_type(
+            **{
+                "plot": "pieplot",
+                "num": 10000,
+                "add": None,
+                "axislabel": "time",
+                "axes": None,
+            }
+        )
+
+        assert num == 10000
+
+    def test_lineplot_add_has_value(self):
+        num = text_add_type(
+            **{
+                "plot": "line",
+                "num": 10000,
+                "add": "Rp (_)",
+                "axislabel": "time",
+                "axes": None,
+            }
+        )
+
+        assert num == "Rp 10000"
+
+    def test_unknown_text_add_argument(self):
+        with pytest.raises(Exception) as exc_info:
+            text_add_type(
+                **{
+                    "plot": "pieplot",
+                    "num": 10000,
+                    "add": [],
+                    "axislabel": "time",
+                    "axes": None,
+                }
+            )
+
+        assert str(exc_info.value) == "Unknown text add argument!"
