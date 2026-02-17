1e90c0d21caae2f430cf0a0e7a0c95ff704720cc
Ghiffary Rifqialdi <grifqialdi@gmail.com>
2026-02-18 00:43:40 +0100

    release: grplot-1.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 DISTNAME = "grplot"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 MAINTAINER = "Ghiffary Rifqialdi"
 MAINTAINER_EMAIL = "grifqialdi@gmail.com"
 DESCRIPTION = "grplot: lazy statistical data visualization"
